a5434dce59cae50f4f8a1bc7dfef5d28fceabecb
ansh goyal <anshmailbox2004@gmail.com>
2024-08-19 00:30:21 +0530

    fe

--- frontend/src/pages/blog.tsx
+++ frontend/src/pages/blog.tsx
@@ -1,7 +1,33 @@
+import Appbar from "../components/Appbar"
+
 export default function Blog(){
     return<>
-    <div>
-        
+    <div className="flex min-h-full">
+        <Appbar/>
+        <div className="w-3/4 pt-16 pl-16 pr-8 pb-10 mt-10">
+        <div className="text-6xl font-bold pb-2">Title</div>
+        <div className="text-slate-500 font-medium pb-6">Posted on August 24, 2022</div>
+        <div className="text-pretty text-slate-800 font-medium">Lorem ipsum, dolor sit amet consectetur adipisicing elit. Expedita distinctio ducimus fugiat optio rerum officia velit harum? Amet architecto omnis quibusdam nemo nam. Quo enim quis quae dolor, eum maiores.
+        Assumenda nam quo maxime. Dicta adipisci consequatur alias nam aperiam nemo voluptas impedit obcaecati quaerat porro iusto dolorem est, error dignissimos animi tempora incidunt aspernatur sequi earum iste ratione veritatis!
+        Esse natus illo et quos, quasi, assumenda quibusdam harum dignissimos blanditiis illum expedita libero quia? Nostrum, obcaecati corporis, recusandae dicta, quaerat quis debitis omnis fugiat veritatis nihil deserunt sint quasi.
+        Tempore laborum explicabo a quod rem rerum deleniti accusamus adipisci reprehenderit hic eligendi, incidunt cupiditate aliquam harum pariatur architecto quas enim! Exercitationem, tempora? Voluptatum placeat aliquid labore itaque unde nisi!
+        Tenetur corrupti vitae veritatis expedita quas deserunt sit mollitia facere. Modi tempore quis error maxime. Laboriosam repellat numquam iusto, illum veritatis nam eius, officia quia voluptatem perferendis recusandae, facere quos.
+        Reiciendis odit saepe, magni vel quia temporibus! Doloremque debitis ab necessitatibus odit sint ex cum reiciendis molestiae praesentium? Esse laborum omnis porro excepturi consectetur laboriosam perspiciatis explicabo libero distinctio eveniet.
+        Velit labore, commodi voluptas at error, ad veritatis dolores sequi exercitationem officiis quia dolorem! Odit odio, deserunt illo praesentium debitis soluta exercitationem? Fuga excepturi suscipit iste at blanditiis ab aperiam?
+        Laboriosam nemo voluptatem harum rem debitis fugiat odit ex vitae voluptate, totam quae nulla quo ratione hic dolores modi, excepturi itaque facere odio adipisci! Delectus fugiat quaerat a neque iure!
+        Culpa dolorum itaque optio accusamus, eveniet delectus repellat sapiente reiciendis voluptas dolore nesciunt ipsam numquam tempora ipsa eum ut alias unde, quas temporibus eius amet, quaerat praesentium ex id? Vitae.
+        Voluptatum blanditiis natus ipsum asperiores rem reprehenderit quod voluptas, nam praesentium incidunt consequatur quo repudiandae minus accusamus, omnis voluptatem iure ratione? Eius, tenetur alias doloribus autem sunt est. Animi, numquam?</div>
+        </div>
+        <div className="w-1/4 flex flex-col p-4 mt-20">
+        <div className="p-2 text-slate-600 font-medium">Author</div>
+        <div className="flex">
+            
+<div className="relative inline-flex items-center justify-center w-10 h-10 overflow-hidden bg-gray-100 rounded-full dark:bg-gray-600 p-2 mt-4">
+    <span className="font-medium text-gray-600 dark:text-gray-300">JL</span>
+</div>
+<div className="mx-4 text-2xl font-bold mt-4">Ansh Goyal</div>
+        </div>
+        </div>
     </div>
     </>
 }
