5acb3c571207fca2d966807f1c1fc06e4bcd0e7c
ansh goyal <anshmailbox2004@gmail.com>
2024-08-20 23:26:00 +0530

    creaet blog page init

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -3,6 +3,7 @@ import SignUp from "./pages/signUp";
 import SignIn from "./pages/signIn";
 import Blog from "./pages/blog";
 import Blogs from "./pages/Blogs";
+import CreateBlog from "./pages/createBlog";
 
 function App() {
   return (
@@ -13,6 +14,8 @@ function App() {
         <Route path="/signup" element={<SignUp/>}></Route>
         <Route path="/blog/:id" element={<Blog/>}></Route>
         <Route path="/blogs" element={<Blogs/>}></Route>
+        <Route path="/create" element={<CreateBlog/>}></Route>
+
         </Routes>      
       </BrowserRouter>
     </>
--- frontend/src/pages/createBlog.tsx
+++ frontend/src/pages/createBlog.tsx
@@ -1,4 +1,48 @@
+import { useState } from "react";
 
-export default function CreateBlog(){
-    
+export default function CreateBlog() {
+  const [blog, setBlog] = useState({
+    title: "",
+    content: "",
+  });
+
+  return (
+    <div>
+      <div className="bg-slate-100 fixed w-full flex justify-between">
+        <div className="flex my-2">
+          <div className="mt-2 px-2 font-serif text-3xl font-bold lg:ml-28">
+            Medium
+          </div>
+          <div className="mt-5 text-sm font-normal">Draft in Anshmailbox</div>
+        </div>
+        <div className="flex my-2 px-2 lg:mr-28">
+          <button className="rounded-full bg-green-600 h-6 w-16 text-sm text-white mt-3 mx-5 font-medium">
+            Publish
+          </button>
+          <div className="relative w-10 h-10 overflow-hidden bg-gray-100 rounded-full dark:bg-gray-600">
+            <svg
+              className="absolute w-12 h-12 text-gray-400 -left-1"
+              fill="currentColor"
+              viewBox="0 0 20 20"
+              xmlns="http://www.w3.org/2000/svg"
+            >
+              <path
+                fill-rule="evenodd"
+                d="M10 9a3 3 0 100-6 3 3 0 000 6zm-7 9a7 7 0 1114 0H3z"
+                clip-rule="evenodd"
+              ></path>
+            </svg>
+          </div>
+        </div>
+      </div>
+      <div className="w-3/4 justify-around border bg-slate-800">
+        <div className="border">
+            <input type="text" placeholder="Title" className="text-2xl border"/>
+        </div>
+        <div>
+            <input type="text" />
+        </div>
+      </div>
+    </div>
+  )
 }
